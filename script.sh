2db10fc46c244e5454440dcbd18b18a8845921a9
robee323 <robee@op.pl>
2023-07-11 00:11:01 +0200

    Parameter validation and timeout mechanism

--- script.sh
+++ script.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/bash
 TARGET="$1"
-wget "https://crt.sh/?q=$TARGET"
+
+# Parameter validation
+if [ $# -ne 1 ]; then
+  echo "To use the script you should run $0 <domain name>"
+  exit 1
+fi
+
+wget --timeout=10 "https://crt.sh/?q=$TARGET"
+
+# Timeout mechanism
+if [ $? -eq 124 ]; then
+  echo "Operation terminated due to timeout."
+  exit 1
+fi
